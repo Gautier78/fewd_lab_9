470346323338f021feba0cd63c8fe12ae47a00eb
Gautier78 <Gautier-lemaitre@hotmail.com>
2020-11-17 16:09:19 +0000

    first commit

---
+++ index.js
@@ -0,0 +1,75 @@
+var Planets = ['Earth', 'Saturn', 'Mars', 'Jupiter', 'Uranus'];
+
+console.log(Planets[0]);
+console.log(Planets[1]);
+console.log(Planets[2]);
+console.log(Planets[3]);
+console.log(Planets[4]);
+// or...
+
+//Planets.forEach(planet => console.log(planet));
+
+/*
+var a = 5,
+b = 'Hello',
+c = "World!",
+d = 10;
+
+console.log(a + b);
+console.log(b + c);
+console.log(a + d);
+*/
+
+/*
+var a = 5,
+b = 10,
+c = "5";
+var x = a;
+
+console.log(a == c);
+
+console.log(a === c);
+
+console.log(a == x);
+
+console.log(a != b);
+
+console.log(a > b);
+
+console.log(a < b);
+
+console.log(a >= b);
+
+console.log(a <= b);
+
+console.log(a >= c);
+
+console.log(a <= c);
+*/
+
+/*
+var color = function(){
+    return 'red';
+};
+
+var result = color();
+console.log(result);
+*/
+
+
+var hello = function(){
+    return 'Hello World!'
+};
+
+console.log(hello);
+
+
+/*
+var hello = function(){
+    return 'Hi!';
+};
+
+var x = hello()
+
+console.log(x);
+*/
